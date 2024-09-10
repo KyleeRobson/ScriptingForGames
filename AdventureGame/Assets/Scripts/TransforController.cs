1e472e4c0f40e620dcede4d813190db1fc3f0653
Kylee Robson <11052487@uvu.edu>
2024-09-09 19:38:53 -0600

    Finished Scripting 2: Lab (2 Hours): Working with GameObjects and Transform

---
+++ AdventureGame/Assets/Scripts/TransforController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransforController : MonoBehaviour
+{
+   
+    // Update is called once per frame
+    private void Update()
+    {
+        // Move the target GameObject
+        var x = Mathf.PingPong(Time.time, 3);
+        var p = new Vector3(0, x, 0);
+        transform.position = p;
+
+        // Rotate the target GameObject
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+    }
+}
